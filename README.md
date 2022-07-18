189f7af1292cc6c1f34811b43eb33ee72562bea9
vinicius morais <vmorais194@gmail.com>
2022-07-18 20:54:55 -0300

    First Day

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1 align="center"> Imersão Java </h1>
+
+![Badge em Desenvolvimento](http://img.shields.io/static/v1?label=STATUS&message=EM%20DESENVOLVIMENTO&color=GREEN&style=for-the-badge)
+![Badge License](https://img.shields.io/github/license/Vinicius-de-Morais/Imersao-Java)
+
+Esse é um projeto de imersão da alura, com o fim de dar um primeiro mergulho e conhecer melhor a linguagem java!
+
+## ✔️ Tecnologia utilizada
+
+- ``Java 17``
+
+## License
+
+Imersao-Java is [MIT licensed](./LICENSE).
---
+++ src/App.java
@@ -0,0 +1,32 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // make an  HTTP requisiton to take JSON info
+        String url = "https://alura-filmes.herokuapp.com/conteudos";
+        URI adress = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient(); // var can be used
+        HttpRequest request = HttpRequest.newBuilder(adress).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // get the concern data (title, image, rate) and keep it in a string
+        var parser = new JsonParser();
+        List<Map<String, String>> filmList = parser.parse(body);
+        
+        // show and manipulate data
+        for (Map<String,String> film : filmList) {
+            System.out.println("\u001b[1m\u001b[46mTítulo:\u001b[m " + film.get("title"));
+            System.out.println("\u001b[1m\u001b[104mCapa:\u001b[m " + film.get("image"));
+            System.out.println("\u001b[1m\u001b[42m\u2B50Classificação:\u001b[m \u2B50" + film.get("imDbRating"));
+            System.out.println(" ");
+        }
+    }
+}
